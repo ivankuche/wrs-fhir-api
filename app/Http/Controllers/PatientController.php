36ac46523a0072d23ec70efb9b4cc980adb213ad
Ivan Kuschevatzky <ivankuche@gmail.com>
2022-03-22 15:42:07 -0300

    Patient get using FHIR format

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -87,6 +87,179 @@ class PatientController extends Controller
     public function show(Patient $patient)
     {
         //
+        /*
+        print_r($patient);
+        die("capo");
+        dd($patient);
+        */
+
+        $response= [
+            "resourceType"=>"Patient",
+            "id"=>$patient->id,
+            "text"=> [
+              "status"=> "generated",
+              "div"=> "generated"
+            ],
+            "identifier"=> [
+              [
+                "use"=> "usual",
+                "type"=> [
+                  "coding"=> [
+                    [
+                      "system"=> "http://terminology.hl7.org/CodeSystem/v2-0203",
+                      "code"=> "MR"
+                    ]
+                  ]
+                ],
+                "system"=>"urn:oid:1.2.36.146.595.217.0.1",
+                "value"=> "12345",
+                "period"=> [
+                  "start"=> "2001-05-06"
+                ],
+                "assigner"=> [
+                  "display"=> "Acme Healthcare"
+                ]
+              ]
+            ],
+            "active"=> ($patient->active?true:false),
+            "name"=> [
+              [
+                "use"=> "official",
+                "family"=> $patient->surname,
+                "given"=> [
+                  $patient->name
+                ]
+              ],
+              [
+                "use"=> "usual",
+                "given"=> [
+                  $patient->name
+                ]
+              ],
+              [
+                "use"=> "maiden",
+                "family"=> "Windsor",
+                "given"=> [
+                  "Peter",
+                  "James"
+                ],
+                "period"=> [
+                  "end"=> "2002"
+                ]
+              ]
+            ],
+            "telecom"=> [
+              [
+                "use"=> "home"
+              ],
+              [
+                "system"=> "phone",
+                "value"=> "(03) 5555 6473",
+                "use"=> "work",
+                "rank"=> 1
+              ],
+              [
+                "system"=> "phone",
+                "value"=> "(03) 3410 5613",
+                "use"=> "mobile",
+                "rank"=> 2
+              ],
+              [
+                "system"=> "phone",
+                "value"=> "(03) 5555 8834",
+                "use"=> "old",
+                "period"=> [
+                  "end"=> "2014"
+                ]
+              ]
+            ],
+            "gender"=> $patient->gender,
+            "birthDate"=> $patient->birthdate,
+            "_birthDate"=> [
+              "extension"=> [
+                [
+                  "url"=> "http://hl7.org/fhir/StructureDefinition/patient-birthTime",
+                  "valueDateTime"=> "1974-12-25T14:35:45-05:00"
+                ]
+              ]
+            ],
+            "deceasedBoolean"=> false,
+            "address"=> [
+              [
+                "use"=> "home",
+                "type"=> "both",
+                "text"=> "534 Erewhon St PeasantVille, Rainbow, Vic  3999",
+                "line"=> [
+                  "534 Erewhon St"
+                ],
+                "city"=> "PleasantVille",
+                "district"=> "Rainbow",
+                "state"=> "Vic",
+                "postalCode"=> "3999",
+                "period"=> [
+                  "start"=> "1974-12-25"
+                ]
+              ]
+            ],
+            "contact"=> [
+              [
+                "relationship"=> [
+                  [
+                    "coding"=> [
+                      [
+                        "system"=> "http://terminology.hl7.org/CodeSystem/v2-0131",
+                        "code"=> "N"
+                      ]
+                    ]
+                  ]
+                ],
+                "name"=> [
+                  "family"=> "du Marché",
+                  "_family"=> [
+                    "extension"=> [
+                      [
+                        "url"=> "http://hl7.org/fhir/StructureDefinition/humanname-own-prefix",
+                        "valueString"=> "VV"
+                      ]
+                    ]
+                  ],
+                  "given"=> [
+                    "Bénédicte"
+                  ]
+                ],
+                "telecom"=> [
+                  [
+                    "system"=> "phone",
+                    "value"=> "+33 (237) 998327"
+                  ]
+                ],
+                "address"=> [
+                  "use"=> "home",
+                  "type"=> "both",
+                  "line"=> [
+                    "534 Erewhon St"
+                  ],
+                  "city"=> "PleasantVille",
+                  "district"=> "Rainbow",
+                  "state"=> "Vic",
+                  "postalCode"=> "3999",
+                  "period"=> [
+                    "start"=> "1974-12-25"
+                  ]
+                ],
+                "gender"=> "female",
+                "period"=> [
+                  "start"=> "2012"
+                ]
+              ]
+            ],
+            "managingOrganization"=> [
+              "reference"=> "Organization/1"
+            ]
+            ];
+
+            return $response;
+
         die("parea");
     }
 
