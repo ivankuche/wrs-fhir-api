9b9b39859b57af775ffecf1489139fd2a3b77b5e
Ivan Kuschevatzky <ivankuche@gmail.com>
2022-03-24 12:02:44 -0300

    Create patient first attempt

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -17,83 +17,15 @@ class PatientController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function index(Request $request)
+
+    private function fhirStructure($patient)
     {
-
-        $patients= Patient::inRandomOrder()->limit(100)->get();
-
-
-        if (!$request->wantsJson())
-            return response()->xml($patients);
-        else
-            return $patients;
-    }
-
-    public function pagination(Request $request)
-    {
-        $patients= Patient::inRandomOrder()->paginate(5);//->get();
-
-
-        if (!$request->wantsJson())
-            return response()->xml($patients);
-        else
-            return $patients;
-    }
-
-    public function getmodified(Request $request)
-    {
-        $patients= Patient::inRandomOrder()->limit(100)->get();
-
-        foreach ($patients as $id=>$data)
-        {
-            $patients[$id]['nationality']='USA';
-        }
-
-
-        if (!$request->wantsJson())
-            return response()->xml($patients);
-        else
-            return $patients;
-    }
-
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-        die("para");
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        return Patient::create(request()->all());
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Patient  $patient
-     * @return \Illuminate\Http\Response
-     */
-    public function show($patientID)
-    {
-        $patient= Patient::findOrFail($patientID);
         $response= [
             "resourceType"=>"Patient",
             "id"=>$patient->id,
             "text"=> [
               "status"=> "generated",
-              "div"=> "generated"
+              "div"=> "blablabla"
             ],
             "identifier"=> [
               [
@@ -131,17 +63,6 @@ class PatientController extends Controller
                   $patient->name
                 ]
               ],
-              [
-                "use"=> "maiden",
-                "family"=> "Windsor",
-                "given"=> [
-                  "Peter",
-                  "James"
-                ],
-                "period"=> [
-                  "end"=> "2002"
-                ]
-              ]
             ],
             "telecom"=> [
               [
@@ -251,8 +172,88 @@ class PatientController extends Controller
             "managingOrganization"=> [
               "reference"=> "Organization/1"
             ]
-            ];
+        ];
 
+        return $response;
+    }
+
+    public function index(Request $request)
+    {
+
+        $patients= Patient::inRandomOrder()->limit(100)->get();
+
+        if (!$request->wantsJson())
+            return response()->xml($patients);
+        else
+            return $patients;
+    }
+
+    public function pagination(Request $request)
+    {
+        $patients= Patient::inRandomOrder()->paginate(5);//->get();
+
+
+        if (!$request->wantsJson())
+            return response()->xml($patients);
+        else
+            return $patients;
+    }
+
+    public function getmodified(Request $request)
+    {
+        $patients= Patient::inRandomOrder()->limit(100)->get();
+
+        foreach ($patients as $id=>$data)
+        {
+            $patients[$id]['nationality']='USA';
+        }
+
+
+        if (!$request->wantsJson())
+            return response()->xml($patients);
+        else
+            return $patients;
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        //
+        print_r($request);
+        Storage::put("public/dumpCreate.txt",print_r($request,true));
+        die();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        //print_r($request);
+        Storage::put("public/dumpCreate.txt",dump($request));
+        die();
+        return Patient::create(request()->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Patient  $patient
+     * @return \Illuminate\Http\Response
+     */
+    public function show($patientID)
+    {
+        $patient= Patient::findOrFail($patientID);
+        $response= $this->fhirStructure($patient);
         $finalResponse= ["resource"=>$response];
 
         return $finalResponse;
@@ -334,151 +335,10 @@ class PatientController extends Controller
 
         foreach($patients as $patient)
         {
-            $response= [
-                "resourceType"=>"Patient",
-                "id"=>$patient->id,
-                "text"=> [
-                  "status"=> "generated",
-                  "div"=> "generated"
-                ],
-                "identifier"=> [
-                  [
-                    "use"=> "usual",
-                    "type"=> [
-                      "coding"=> [
-                        [
-                          "system"=> "http://terminology.hl7.org/CodeSystem/v2-0203",
-                          "code"=> "MR"
-                        ]
-                      ]
-                    ],
-                    "system"=>"urn:oid:1.2.36.146.595.217.0.1",
-                    "value"=> "12345",
-                    "period"=> [
-                      "start"=> "2001-05-06"
-                    ],
-                    "assigner"=> [
-                      "display"=> "Acme Healthcare"
-                    ]
-                  ]
-                ],
-                "active"=> ($patient->active?true:false),
-                "name"=> [
-                  [
-                    "use"=> "official",
-                    "family"=> $patient->surname,
-                    "given"=> [
-                      $patient->name
-                    ]
-                  ],
-                  [
-                    "use"=> "usual",
-                    "given"=> [
-                      $patient->name
-                    ]
-                  ],
-                ],
-                "telecom"=> [
-                  [
-                    "use"=> "home"
-                  ],
-                  [
-                    "system"=> "phone",
-                    "value"=> "(03) 5555 6473",
-                    "use"=> "work",
-                    "rank"=> 1
-                  ],
-                ],
-                "gender"=> $patient->gender,
-                "birthDate"=> $patient->birthdate,
-                "_birthDate"=> [
-                  "extension"=> [
-                    [
-                      "url"=> "http://hl7.org/fhir/StructureDefinition/patient-birthTime",
-                      "valueDateTime"=> "1974-12-25T14:35:45-05:00"
-                    ]
-                  ]
-                ],
-                "deceasedBoolean"=> false,
-                "address"=> [
-                  [
-                    "use"=> "home",
-                    "type"=> "both",
-                    "text"=> "534 Erewhon St PeasantVille, Rainbow, Vic  3999",
-                    "line"=> [
-                      "534 Erewhon St"
-                    ],
-                    "city"=> "PleasantVille",
-                    "district"=> "Rainbow",
-                    "state"=> "Vic",
-                    "postalCode"=> "3999",
-                    "period"=> [
-                      "start"=> "1974-12-25"
-                    ]
-                  ]
-                ],
-                "contact"=> [
-                  [
-                    "relationship"=> [
-                      [
-                        "coding"=> [
-                          [
-                            "system"=> "http://terminology.hl7.org/CodeSystem/v2-0131",
-                            "code"=> "N"
-                          ]
-                        ]
-                      ]
-                    ],
-                    "name"=> [
-                      "family"=> "du Marché",
-                      "_family"=> [
-                        "extension"=> [
-                          [
-                            "url"=> "http://hl7.org/fhir/StructureDefinition/humanname-own-prefix",
-                            "valueString"=> "VV"
-                          ]
-                        ]
-                      ],
-                      "given"=> [
-                        "Bénédicte"
-                      ]
-                    ],
-                    "telecom"=> [
-                      [
-                        "system"=> "phone",
-                        "value"=> "+33 (237) 998327"
-                      ]
-                    ],
-                    "address"=> [
-                      "use"=> "home",
-                      "type"=> "both",
-                      "line"=> [
-                        "534 Erewhon St"
-                      ],
-                      "city"=> "PleasantVille",
-                      "district"=> "Rainbow",
-                      "state"=> "Vic",
-                      "postalCode"=> "3999",
-                      "period"=> [
-                        "start"=> "1974-12-25"
-                      ]
-                    ],
-                    "gender"=> "female",
-                    "period"=> [
-                      "start"=> "2012"
-                    ]
-                  ]
-                ],
-                "managingOrganization"=> [
-                  "reference"=> "Organization/1"
-                ]
-            ];
-
+            $response= $this->fhirStructure($patient);
             $return["resource"][]= $response;
 //            $finalResponse= ["resource"=>$response];
         }
-
-
 
         return $return;
     }
