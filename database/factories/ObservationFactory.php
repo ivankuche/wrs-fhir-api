9ca3a19874543681e1c09adb89fddf462f2185c4
Ivan Kuschevatzky <ivankuche@gmail.com>
2022-04-11 20:53:56 -0300

    Agregado de Observation

--- database/factories/ObservationFactory.php
+++ database/factories/ObservationFactory.php
@@ -19,7 +19,7 @@ class ObservationFactory extends Factory
         return [
             'status' => 'final',
             'issued'=> $this->faker->dateTime(),
-            "effectiveDateTime"  => $this->faker->date()
+            "effectiveDateTime"  => $this->faker->date('2020-m-d')
         ];
     }
 }
