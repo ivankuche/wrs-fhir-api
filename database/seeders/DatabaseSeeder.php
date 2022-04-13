61764d4fb1b2efcef9f893f1b0f5c6434d2e362c
Ivan Kuschevatzky <ivankuche@gmail.com>
2022-04-13 08:44:27 -0300

    Agregando ejemplos de Clinical Notes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -196,7 +196,7 @@ class DatabaseSeeder extends Seeder
                 'type'=>'Observation'
             ],
             "presentedForm" => [
-                "url"=>"http://www.demoreport.com/demoreport",
+                "url"=>"http://www.demoreport.com/demoreport/",
             ],
             'category'=> [
                 "coding" => [
@@ -974,10 +974,12 @@ class DatabaseSeeder extends Seeder
     {
         $documents= [
             "11488-4"=>"Consult note",
+            /*
             "18842-5"=>"Discharge summary",
             "34117-2"=>"History and physical note",
             "28570-0"=>"Procedure note",
             "11506-3"=>"Progress note"
+            */
         ];
 
         foreach ($documents as $typeID=>$typeDescription)
@@ -1153,7 +1155,7 @@ class DatabaseSeeder extends Seeder
         });
         $this->medication();
         $this->organizationPayer1();
-        //$this->extraDocuments();
+        $this->extraDocuments();
         //$this->extraReports();
         $this->dataAbsent();
 
