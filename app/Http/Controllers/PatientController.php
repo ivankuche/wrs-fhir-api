63c75ecff386596f2f265964dbb72a70cbebd1b5
Ivan Kuschevatzky <ivankuche@gmail.com>
2022-04-15 13:50:30 -0300

    Gender Identity for Patients

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -124,7 +124,7 @@ class PatientController extends Controller
                     ],
                     [
                         "url"=>"http://hl7.org/fhir/StructureDefinition/patient-genderIdentity",
-                        "valueString" => $patient->genderIdentity
+                        "valueCodeableConcept" => $patient->genderIdentity
                     ]
 
                 ],
