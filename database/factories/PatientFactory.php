8b97384a779cd4a03b9910c31f7101afd5ff2684
Kuche <ivankuche@gmail.com>
2022-08-18 11:04:10 -0300

    Adding patient suffix and previous name

--- database/factories/PatientFactory.php
+++ database/factories/PatientFactory.php
@@ -21,7 +21,7 @@ class PatientFactory extends Factory
         $gender = $this->faker->randomElement(['male', 'female']);
         $genderIdentity = $this->faker->randomElement(['male','female','non-binary','transgender-male','transgender-female', 'other', 'non-disclose']);
 
-        $suffix = $this->faker->randomElement(['Jr','2nd','',"The 3rd"]);
+        $suffix = $this->faker->randomElement(['Jr','2nd',"The 3rd"]);
 
 
         $name= $this->faker->firstName($gender);
@@ -168,7 +168,7 @@ class PatientFactory extends Factory
                     'text'=>$name." ".$surname,
                     'family'=>$surname,
                     'given'=>[$name],
-                    "suffix"=>[$suffix],
+                    'suffix'=>[$suffix],
                 ],
                 $previousName],
             'telecom'=> [[
